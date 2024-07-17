7c552f30995438a19289861a4edb639fec1215a0
Jeevan <work.jeevansp@gmail.com>
2024-07-17 15:59:31 -0400

    v1

--- script.js
+++ script.js
@@ -1,38 +1,72 @@
-const add = function(a, b) {
-    return a + b;
-  };
-  
-  const subtract = function(a, b) {
-      return a - b;
-  };
-  
-  const sum = function(array) {
-      return array.reduce((total, current) => {
-      return (total + current);
-    }, 0)
-  };
-  
-  const multiply = function(array) {
-    return array.reduce((product, current) => {
-      return product * current;
-    }, 1)
-  };
-  
-  const power = function(a, b) {
-      return Math.pow(a, b);
-  };
-  
-  const factorial = function (n) {
-    if (n === 0) return 1;
-    let product = 1;
-    for (let i = n; i > 0; i--) {
-      product *= i;
-    }
-    return product;
-  };
-  
-  const divide = function(array) {
-    return array.reduce((quotient, current) => {
-      return quotient / current;
-    })
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const power = (a, b) => Math.pow(a, b);
+const factorial = (a, b) => a % b;
+const root = (a, b) => a/b;
+
+
+let firstOperand = null;
+let operator = null;
+
+const container = document.querySelector("#calculatorContainer");
+const screen = document.querySelector("#screen");
+
+function toDisplay(input) {
+  screen.value += input;
+}
+
+function resetFunction() {
+  screen.value = "";
+  firstOperand = null;
+  operator = null;
+}
+
+function delChar() {
+  screen.value = screen.value.length === 1 ? "" : screen.value.slice(0, -1);
+}
+function setOperator(op) {
+  if (firstOperand === null) {
+    firstOperand = parseFloat(screen.value);
+    operator = op;
+    screen.value = "";
   }
+}
+
+function calculate() {
+  if (firstOperand !== null && operator !== null) {
+    const secondOperand = parseFloat(screen.value);
+    let result;
+    switch (operator) {
+      case '+':
+        result = add(firstOperand, secondOperand);
+        break;
+      case '-':
+        result = subtract(firstOperand, secondOperand);
+        break;
+      case 'x':
+        result = multiply(firstOperand, secondOperand);
+        break;
+      case '/':
+        result = divide(firstOperand, secondOperand).toFixed(2);
+        if (secondOperand === 0) {
+        result = "division by 0 is not allowed"
+        }
+        break;
+      case '^':
+        result = power(firstOperand, secondOperand);
+        break;
+      case '%':
+        result = factorial(firstOperand, secondOperand);
+        break;
+      case '√':
+        result = Math.sqrt(firstOperand, secondOperand)
+      default:
+        result = "Error";
+    }
+    screen.value = result;
+    firstOperand = null;
+    operator = null;
+  }
+}
